ea52d5c09429113dd7e8fde4bf345252e51594d1
John Ansley Rocel <estevesjancen388@gmail.com>
2026-09-25 21:07:50 +0800

    Replace the Laravel favicon with the ISU seal
    Generate favicon.ico (16, 32, 48px), a 192px favicon.png, and a 180px
    apple-touch-icon on white from the university logo. Drop the Laravel
    favicon.svg, which browsers would otherwise prefer.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         <title>{{ __('Welcome') }} - {{ config('app.name', 'Laravel') }}</title>
 
         <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="icon" href="/favicon.png" type="image/png" sizes="192x192">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         @fonts
